132271b9f6912543b4977a755836d47dcad6e490
Dakota Bourne <dakota.bourne0210@gmail.com>
2024-12-15 22:33:06 -0600

    adjust connection dialog box

--- components/ConnectionBanner.tsx
+++ components/ConnectionBanner.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { GestureResponderEvent, Pressable, Text } from 'react-native';
 import { Settings } from '@/lib/icons/Settings';
-import {ipCheck} from '@/lib/regex';
+import { ipCheck } from '@/lib/regex';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
 import { Button } from './ui/button';
@@ -31,14 +31,16 @@ export const ConnectionBanner: React.FC<ConnectionBanner.Props> = ({ connectedSe
             setError(true);
             return;
         }
-        if (!ipAddress.match(ipCheck)) {
-            console.log('ip address did not meet requirements');
-            event.preventDefault();
-            setError(true);
-            return;
-        }
         onPress(ipAddress);
     }
+
+    React.useEffect(() => {
+        if (!ipAddress?.match(ipCheck)) {
+            setError(true);
+        } else {
+            setError(false);
+        }
+    }, [ipAddress]);
 
     return (
         <Dialog open={open} onOpenChange={setOpen} style={{ width: '100%' }}>
@@ -50,29 +52,28 @@ export const ConnectionBanner: React.FC<ConnectionBanner.Props> = ({ connectedSe
                     </Pressable>
                 </LinearGradient>
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent style={{ marginBottom: 120 }}>
                 <DialogHeader>
                     <DialogTitle>Edit server ip</DialogTitle>
                     <DialogDescription>
                         Put in the ip of the computer running the server. Click save when you're done.
                     </DialogDescription>
                 </DialogHeader>
-                    <>
-                        <Label nativeID='ip-label'>Enter ip</Label>
-                        <Input
-                            defaultValue={ipAddress}
-                            placeholder='192.168.1.200...'
-                            onChangeText={setIpAddress}
-                            aria-labelledby='ip-label'
-                            aria-invalid={error}
-                            aria-errormessage='ip-error'
-                        />
-                        {error ? <Animated.View entering={FlipInXDown} exiting={FlipOutXDown} style={{flexDirection: 'row', alignItems: 'flex-start', alignContent: 'flex-start', gap: 4}}><Info color='red' size={16} style={{marginTop: 3}} /><Label nativeID='ip-error' style={{ color: 'red' }}>The ip you submitted is invalid, please resubmit with a valid ip</Label></Animated.View> : null}
-                    </>
-
+                <>
+                    <Label nativeID='ip-label'>Enter ip</Label>
+                    <Input
+                        defaultValue={ipAddress}
+                        placeholder='192.168.1.200...'
+                        onChangeText={setIpAddress}
+                        aria-labelledby='ip-label'
+                        aria-invalid={error}
+                        aria-errormessage='ip-error'
+                    />
+                    {error ? <Animated.View entering={FlipInXDown} exiting={FlipOutXDown} style={{ flexDirection: 'row', alignItems: 'flex-start', alignContent: 'flex-start', gap: 4 }}><Info color='red' size={16} style={{ marginTop: 3 }} /><Label nativeID='ip-error' style={{ color: 'red' }}>The ip you submitted is invalid, please resubmit with a valid ip</Label></Animated.View> : null}
+                </>
                 <DialogFooter>
                     <DialogClose asChild onPress={handleSubmit}>
-                        <Button>
+                        <Button disabled={error}>
                             <Text>Save</Text>
                         </Button>
                     </DialogClose>
