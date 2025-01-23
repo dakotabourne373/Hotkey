188c11c18c8aca246ff8810efdfee05bba7537f2
Dakota Bourne <dakota.bourne0210@gmail.com>
2025-01-23 15:20:15 -0600

    more workflow changes

--- hotkey-server/index.js
+++ hotkey-server/index.js
@@ -8,7 +8,7 @@ let localIp = "localhost";
 
 for (const name of Object.keys(interfaces)) {
   for (const iface of interfaces[name]) {
-    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses.
     if (iface.family === "IPv4" && !iface.internal) {
       localIp = iface.address;
     }
